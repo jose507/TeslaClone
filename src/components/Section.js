ff1285ec3099ace6327f3000c61d05bb19e9eeed
Jose Regalado <jlrmyt@gmail.com>
2021-10-30 17:47:21 -0500

    redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
+
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImage }) {
     return (
         <Wrap bgImage={ backgroundImage }>
-            <ItemText>
-                <h1>{  title }</h1>
-                <p>{ description }</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{  title }</h1>
+                    <p>{ description }</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        { leftBtnText }
-                    </LeftButton>
-                    { rightBtnText &&
-                        <RightButton>
-                        { rightBtnText }
-                        </RightButton>
-                    }
-                </ButtonGroup>
-                <DownArrow src='/images/down-arrow.svg' />
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            { leftBtnText }
+                        </LeftButton>
+                        { rightBtnText &&
+                            <RightButton>
+                            { rightBtnText }
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                    <DownArrow src='/images/down-arrow.svg' />
+                </Fade>
             </Buttons>
         </Wrap>
     )
